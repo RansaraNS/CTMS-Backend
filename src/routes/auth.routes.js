2c31e8025c757808dc2c5b246498c80d4c7117b3
IT22269652 <IT22269652@my.sliit.lk>
2025-09-26 12:57:17 +0530

    add backend parts for creating HR part

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,29 @@
 import express from "express";
-import {creatAdmin, registerHR, login } from "../controllers/auth.controller.js";
+import { creatAdmin, registerHR, login, getHrs, deleteHr, updateHr, getLatestHrs } from "../controllers/auth.controller.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-
-router.post("/create-admin",creatAdmin);
+router.post("/create-admin", creatAdmin);
 
 // Admin creates HR
 router.post("/register-hr", requireAuth, registerHR);
 
 // Login (Admin/HR)
 router.post("/login", login);
+
+
+//new
+// Get all HRs (Admin only)
+router.get("/hrs", requireAuth, getHrs);
+
+// Delete HR (Admin only)
+router.delete("/hrs/:id", requireAuth, deleteHr);
+
+// Update HR (Admin only)
+router.put("/hrs/:id", requireAuth, updateHr);
+
+// get latest hr 
+router.get("/latest-hrs", requireAuth, getLatestHrs);
 
 export default router;
